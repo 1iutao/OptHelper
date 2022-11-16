7464b7a1f1c438b53e52236b848b94ed4e776f3f
liutao <545424546@qq.com>
2022-11-16 18:16:10 +0800

    20221116

--- opt-helper-web/vue.config.js
+++ opt-helper-web/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //打开浏览器窗口
+  devServer :{
+    host :'localhost',
+    port : 8080,
+    open : true
+  },
 })
